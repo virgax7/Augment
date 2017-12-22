5a8b599fb20a41c331a0349709ffadea2a161dc6
virgax7 <pyoungx7@gmail.com>
2017-12-21 21:04:00 -0500

    basic Hello service implemented.
    -- we need the two jars because tomcat needs them for xhtml files

---
+++ src/main/java/com/augment/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.augment.controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloController {
+    public String sayHello() {
+        return "HELLO WORLD";
+    }
+}
