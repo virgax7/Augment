38b216362771658719fdfd5b160aba58a18f7c18
virgax7 <pyoungx7@gmail.com>
2017-12-28 22:30:18 -0500

    basic edit and info cell added...

--- web/resources/js/schedule/future.js
+++ web/resources/js/schedule/future.js
@@ -1,15 +1,35 @@
-var cells = document.getElementsByClassName("cell");
+var cells = document.getElementsByClassName("taskWrapper");
 for (var i = 0; i < cells.length; i++) {
     cells[i].setAttribute("draggable", true);
     cells[i].addEventListener("dragstart", dragStart);
 }
 
-var dropZones = document.getElementsByClassName("dropZone");
+var dropZones = document.getElementsByClassName("dropZoneCell");
 for (var i = 0; i < dropZones.length; i++) {
     dropZones[i].addEventListener("drop", dragDrop);
-    dropZones[i].addEventListener("dragover", function() {
+    dropZones[i].addEventListener("dragover", function () {
         event.preventDefault();
     });
+}
+
+var editButtons = document.getElementsByClassName("editButton");
+for (var i = 0; i < dropZones.length; i++) {
+    editButtons[i].addEventListener("click", showEditCell);
+}
+
+// find out a way to make this replace content with the task element you click
+function showEditCell() {
+    var editCell =
+        '<div id="editCell">' +
+            '<button id="closeEditCellButton"></button>' +
+        '</div>'
+    document.getElementById("mainTable").insertAdjacentHTML('beforeend', editCell);
+    document.getElementById("closeEditCellButton").addEventListener("click", closeEditCell);
+}
+
+function closeEditCell() {
+    var editCell = document.getElementById("editCell");
+    editCell.parentNode.removeChild(editCell);
 }
 
 function dragStart(event) {
